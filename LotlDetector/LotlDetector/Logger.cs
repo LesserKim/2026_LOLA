5fc1ef9cc9702c53f1116998b206f449487e9cfd
LesserKim <lesser1217@naver.com>
2026-01-30 00:18:09 +0900

    add Logger.cs and integrate logging into main

---
+++ LotlDetector/LotlDetector/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsTCPIP;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Timers;
+
+namespace LotlDetector
+{
+    public class Logger
+    {
+        private static string logPath = "DetectionLog.txt";
+        public static void WriteLog(string img, string parent, string cmd, string decoded, int score)
+        {
+            string logEntity = $@"
+            [Time]: {DateTime.Now:yyyy-MM-dd HH: mm: ss} 
+            [status] : Blocked
+            [Process]: {img}
+            [Parent Process]: {parent}
+            [Command Line]: {cmd} 
+            [score] : {score}
+            [result] : {(string.IsNullOrEmpty(decoded) ? "N/A":decoded)}
+           ---------------------------------------------- ";
+
+            try
+            {
+                File.AppendAllText(logPath, logEntity + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Log Save Failed : {ex.Message}");
+            }
+        }
+    }
+}
--- LotlDetector/LotlDetector/Program.cs
+++ LotlDetector/LotlDetector/Program.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 
 var mapper = new ProcessMapper();
 var analyzer = new Analyzer();
+//var logger = new Logger();
 string sessionName = "LotL-EDR";
 
 //커널 이벤트 세션 
@@ -17,11 +18,38 @@ using (var session = new TraceEventSession(sessionName))
     {
         mapper.Add(Data.ProcessID, Data.ImageFileName);
         string pName = mapper.GetParentName(Data.ParentID);
-
         int score = analyzer.CalculateScore(Data.ImageFileName, pName, Data.CommandLine, out List<string> reasons);
         string decodedCmd = analyzer.DecodePowerShell(Data.CommandLine);
 
         if (score > 0)
+        {
+            PrintAlert(Data.ImageFileName, pName, Data.CommandLine, decodedCmd, score, reasons);
+        }
+
+        if (score >= 30)
+        {
+            try
+            {
+                var targetProcess = System.Diagnostics.Process.GetProcessById(Data.ProcessID);
+                targetProcess.Kill();
+
+                Logger.WriteLog(Data.ImageFileName, pName, Data.CommandLine, decodedCmd, score);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n차단됨. 위험 행위 감지, 프로세스 종료.");
+                Console.WriteLine($"차단 대상: {Data.ImageFileName} PID: {Data.ProcessID}");
+                Console.ResetColor();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"이미 종료된 프로세스입니다. : {Data.ImageFileName}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"차단 실패 :{ex.Message}");
+            }
+        }
+        if(score > 0)
         {
             PrintAlert(Data.ImageFileName, pName, Data.CommandLine, decodedCmd, score, reasons);
         }
